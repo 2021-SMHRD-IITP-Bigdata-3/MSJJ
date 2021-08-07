55401709b36477a4adfca785125cfd1b1b21336a
smhrd <smhrd@121.147.0.252>
2021-08-07 12:30:11 +0900

    aa

---
+++ Flower/WebContent/insertProduct1.sql
@@ -0,0 +1,146 @@
+insert into product 
+values(201001, '노랑장미','노랑노랑 나의 사랑', 2 ,80000, '"./img/1/1/yr_2.jpg"', 'T', 1);
+insert into product 
+values(201002, '노랑장미_리안서스','영원한 사랑', 2 ,80000, '"./img/1/1/yr_ls_2.jpg"', 'T', 1);
+insert into product 
+values(201003, '노랑장미_소국_카네이션','영원한 나의 존경', 2 ,80000, '"./img/1/1/yr_sf_c_2.jpg"', 'T', 1);
+insert into product 
+values(201004, '노랑장미_하얀안개','영원한 사랑을 믿어요', 2 ,80000, '"./img/1/1/yr_wm_2.jpg"', 'T', 1);
+insert into product 
+values(201005, '리시안_소국_스토크','영원한 사랑은 진심입니다', 2 ,80000, '"./img/1/1/yr_sf_st_2.jpg"', 'T', 1);
+insert into product 
+values(201006, '노랑장미_리시안_소국','사랑의 진심', 2 ,80000, '"./img/1/1/yr_ls_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201007, '리시안_소국','사랑의 진심', 2 ,70000, '"./img/1/1/ls_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201008, '보라장미_리시안_소국','사랑의 진심', 2 ,70000, '"./img/1/1/pur_ls_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201009, '분홍장미_보라장미_리시안_소국','나의 행복은 당신', 2 ,70000, '"./img/1/1/pr_pur_ls_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201010, '분홍장미_하얀안개','나의 행복은 당신', 2 ,70000, '"./img/1/1/pr_wm_2.jpg"', 'T', 1);
+insert into product 
+values(201011, '분홍장미','나의 행복은 당신', 2 ,70000, '"./img/1/1/pr_2.jpg"', 'T', 1);
+insert into product 
+values(201012, '분홍장미_리시안_소국','나의 행복은 당신', 2 ,70000, '"./img/1/1/pr_ls_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201013, '분홍장미_리시안_거베라_소국','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/pr_ls_g_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201014, '분홍장미_리시안_거베라_소국','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/pr_ls_g_sf_22.jpg"', 'T', 1);
+insert into product 
+values(201015, '분홍장미_주황장미_소국_거베라_카네이션','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/pr_or_sf_g_c_2.jpg"', 'T', 1);
+insert into product 
+values(201016, '빨강장미','불타는 나의 사랑', 2 ,70000, '"./img/1/1/rr_2.jpg"', 'T', 1);
+insert into product 
+values(201017, '빨강장미_하얀안개','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/rr_wm_2.jpg"', 'T', 1);
+insert into product 
+values(201018, '빨강장미_리시안_하얀안개','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/rr_ls_wm_2.jpg"', 'T', 1);
+insert into product 
+values(201019, '빨강장미_리시안','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/rr_ls_2.jpg"', 'T', 1);
+insert into product 
+values(201020, '빨강장미_분홍장미_수국_소국','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/rr_pr_h_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201021, '빨강장미_리시안_거베라_소국_수국','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/rr_ls_g_sf_h_2.jpg"', 'T', 1);
+insert into product 
+values(201022, '빨강장미_분홍장미_리시안','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/rr_pr_ls_2.jpg"', 'T', 1);
+insert into product 
+values(201023, '빨강장미_소국','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/rr_sf_2.jpg"', 'T', 1);
+insert into product 
+values(201024, '주황장미','수줍은 나의 사랑', 2 ,70000, '"./img/1/1/or_2.jpg"', 'T', 1);
+insert into product 
+values(201025, '분홍장미_리시안_거베라_소국','행복한 나의 진심', 2 ,70000, '"./img/1/1/pr_ls_g_sf_22.jpg"', 'T', 1);
+insert into product 
+values(201026, '분홍장미_리시안_파랑안개','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/pr_ls_bm_2.jpg"', 'T', 1);
+insert into product 
+values(301001, '노랑장미_노랑거베라_스토크','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/301001.jpg"', 'T', 1);
+insert into product 
+values(301002, '노랑장미','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/301002.jpg"', 'T', 1);
+insert into product 
+values(301003, '노랑장미_리시안','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/301003.jpg"', 'T', 1);
+insert into product 
+values(301004, '노랑장미_리시안_소국','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/301004.jpg"', 'T', 1);
+insert into product 
+values(301005, '노랑장미_하얀안개','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/301005.jpg"', 'T', 1);
+insert into product 
+values(301006, '분홍장미_리시안_소국','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/301006.jpg"', 'T', 1);
+insert into product 
+values(301007, '빨강장미_하얀안개','내 마음의 진심은 당신', 2 ,70000, '"./img/1/1/301007.jpg"', 'T', 1);
+insert into product 
+values(301008, '거베라_소국','나의 진심은 당신', 2 ,70000, '"./img/1/1/301008.jpg"', 'T', 1);
+insert into product 
+values(301009, '리시안_소국','나의 진심은 당신', 2 ,70000, '"./img/1/1/301009.jpg"', 'T', 1);
+insert into product 
+values(301010, '리시안','나의 진심은 당신', 2 ,70000, '"./img/1/1/301010.jpg"', 'T', 1);
+insert into product 
+values(301011, '분홍장미','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301011.jpg"', 'T', 1);
+insert into product 
+values(301012, '분홍장미_리시안','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301012.jpg"', 'T', 1);
+insert into product 
+values(301013, '분홍장미_하얀안개','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301013.jpg"', 'T', 1);
+insert into product 
+values(301014, '분홍장미_리시안_거베라_소국','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301014.jpg"', 'T', 1);
+insert into product 
+values(301015, '분홍장미_리시안_백합_카네이션','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301015.jpg"', 'T', 1);
+insert into product 
+values(301016, '분홍장미_리시안_소국','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301016.jpg"', 'T', 1);
+insert into product 
+values(301017, '분홍장미_리시안_스토크_카네이션','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301017.jpg"', 'T', 1);
+insert into product 
+values(301018, '분홍장미_보라장미_리시안_소국_카네이션','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301018.jpg"', 'T', 1);
+insert into product 
+values(301019, '분홍장미_소국','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301019.jpg"', 'T', 1);
+insert into product 
+values(301020, '분홍장미_리시안_수국','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301020.jpg"', 'T', 1);
+insert into product 
+values(301021, '분홍장미_카네이션_소국','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301021.jpg"', 'T', 1);
+insert into product 
+values(301022, '분홍장미_하얀장미_리시안','행복한 나의 사랑', 2 ,70000, '"./img/1/1/301022.jpg"', 'T', 1);
+insert into product 
+values(301023, '빨강장미','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301023.jpg"', 'T', 1);
+insert into product 
+values(301024, '빨강장미_리시안','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301024.jpg"', 'T', 1);
+insert into product 
+values(301025, '빨강장미_하얀안개','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301025.jpg"', 'T', 1);
+insert into product 
+values(301026, '빨강장미_카네이션','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301026.jpg"', 'T', 1);
+insert into product 
+values(301027, '빨강장미_거베라_소국','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301027.jpg"', 'T', 1);
+insert into product 
+values(301028, '빨강장미_리시안_소국','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301028.jpg"', 'T', 1);
+insert into product 
+values(301029, '빨강장미_소국_거베라_카네이션','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301029.jpg"', 'T', 1);
+insert into product 
+values(301030, '빨강장미_분홍장미_소국','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301030.jpg"', 'T', 1);
+insert into product 
+values(301031, '빨강장미_분홍장미_소국_스토크','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301031.jpg"', 'T', 1);
+insert into product 
+values(301032, '빨강장미_분홍장미_수국_소국','뜨거운 나의 사랑', 2 ,70000, '"./img/1/1/301032.jpg"', 'T', 1);
+insert into product 
+values(301033, '소국','나의 진심', 2 ,70000, '"./img/1/1/301033.jpg"', 'T', 1);
+insert into product 
+values(301034, '리시안_소국_수국','나의 사랑, 나의 진심', 2 ,70000, '"./img/1/1/301034.jpg"', 'T', 1);
+insert into product 
+values(301035, '주황장미_거베라_카네이션_소국','수줍은 나의 사랑', 2 ,70000, '"./img/1/1/301035.jpg"', 'T', 1);
+insert into product 
+values(301036, '카네이션','존경과 감사를 담아', 2 ,70000, '"./img/1/1/301036.jpg"', 'T', 1);
+insert into product 
+values(301037, '카네이션_소국','존경과 감사를 담아', 2 ,70000, '"./img/1/1/301037.jpg"', 'T', 1);
+insert into product 
+values(301038, '카네이션_하얀안개','존경과 감사를 담아', 2 ,70000, '"./img/1/1/301038.jpg"', 'T', 1);
+insert into product 
+values(401001, '노랑장미','노랑노랑 나의 사랑', 2 ,90000, '"./img/1/1/401001.jpg"', 'T', 1);
+insert into product 
+values(401002, '분홍장미','핑크빛 사랑', 2 ,90000, '"./img/1/1/401002.jpg"', 'T', 1);
+insert into product 
+values(401003, '분홍장미_리시안_카네이션','핑크빛 마음', 2 ,90000, '"./img/1/1/401003.jpg"', 'T', 1);
+insert into product 
+values(401004, '분홍장미_리시안_거베라_카네이션','핑크빛 마음', 2 ,90000, '"./img/1/1/401004.jpg"', 'T', 1);
+insert into product 
+values(401005, '분홍장미_리시안_거베라_소국_수국','핑크빛 진심', 2 ,90000, '"./img/1/1/401005.jpg"', 'T', 1);
+insert into product 
+values(401006, '빨강장미_리시안_거베라_카네이션','뜨거운 진심', 2 ,90000, '"./img/1/1/401006.jpg"', 'T', 1);
+insert into product 
+values(401007, '빨강장미_리시안_소국','뜨거운 진심', 2 ,90000, '"./img/1/1/401007.jpg"', 'T', 1);
+insert into product 
+values(401008, '빨강장미_소국','뜨거운 진심', 2 ,90000, '"./img/1/1/401008.jpg"', 'T', 1);
+insert into product 
+values(401009, '소국','뜨거운 진심', 2 ,90000, '"./img/1/1/401009.jpg"', 'T', 1);
