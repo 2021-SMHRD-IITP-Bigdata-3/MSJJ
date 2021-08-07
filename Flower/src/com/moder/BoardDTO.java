76fea8e57dec5b4a6f5eeb0bc2a3a5f705b2e725
woojin <us8432@gmail.com>
2021-08-07 11:40:38 +0900

    boarddto

--- Flower/src/com/moder/BoardDTO.java
+++ Flower/src/com/moder/BoardDTO.java
@@ -2,56 +2,65 @@ package com.moder;
 
 public class BoardDTO {
 	
-	private int num;
-	private String email;
-	private String content;
-	private String b_date;
+	private int board_number;
+	private String board_content;
+	private String board_flower;
+	private String board_store;
+	private String board_member;
+	private String board_date;
+	private String board_score;
 	private String name;
-	private String tel;
+	private String email;
 	private String pw;
+	private String tel;
 	
-	
+	public int getBoard_number() {
+		return board_number;
+	}
+	public void setBoard_number(int board_number) {
+		this.board_number = board_number;
+	}
+	public String getBoard_content() {
+		return board_content;
+	}
+	public void setBoard_content(String board_content) {
+		this.board_content = board_content;
+	}
+	public String getBoard_flower() {
+		return board_flower;
+	}
+	public void setBoard_flower(String board_flower) {
+		this.board_flower = board_flower;
+	}
+	public String getBoard_store() {
+		return board_store;
+	}
+	public void setBoard_store(String board_store) {
+		this.board_store = board_store;
+	}
+	public String getBoard_member() {
+		return board_member;
+	}
+	public void setBoard_member(String board_member) {
+		this.board_member = board_member;
+	}
+	public String getBoard_date() {
+		return board_date;
+	}
+	public void setBoard_date(String board_date) {
+		this.board_date = board_date;
+	}
+	public String getBoard_score() {
+		return board_score;
+	}
+	public void setBoard_score(String board_score) {
+		this.board_score = board_score;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getPw() {
-		return pw;
-	}
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
-	public BoardDTO(String email, String content) {
-		super();
-		this.email = email;
-		this.content = content;
-	}
-	public BoardDTO(String pw, String name, String tel) {
-		super();
-		this.pw = pw;
-		this.name = name;
-		this.tel = tel;
-	}
-	public BoardDTO(int num, String email, String content, String b_date) {
-		super();
-		this.num = num;
-		this.email = email;
-		this.content = content;
-		this.b_date = b_date;
-	}
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
 	}
 	public String getEmail() {
 		return email;
@@ -59,17 +68,23 @@ public class BoardDTO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getContent() {
-		return content;
+	public String getPw() {
+		return pw;
 	}
-	public void setContent(String content) {
-		this.content = content;
+	public void setPw(String pw) {
+		this.pw = pw;
 	}
-	public String getB_date() {
-		return b_date;
+	public String getTel() {
+		return tel;
 	}
-	public void setB_date(String b_date) {
-		this.b_date = b_date;
+	public void setTel(String tel) {
+		this.tel = tel;
 	}
 	
+	
+	
+	
+
+	
+
 }
